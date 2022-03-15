4b27766475bea46f4e1f1285ddbf462dbb570185
aofur3957 <sunggin69@gmail.com>
2022-03-15 14:14:58 +0900

    my skill layout

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 const title = document.querySelector('#visual h1');
 const titleText = title.innerText;
 
-const scroll = window.scrollY;
 const section = document.querySelectorAll('section');
+const sectionOffset = [];
+const num = document.querySelectorAll('#intro span');
 
+const scrollBox = document.querySelector('.scroll dd');
 
+// title 및 subtitle 애니메이션 
 let titleSplit = titleText.split('').join('</span><span>');
 titleSplit = `<span>${titleSplit}</span>`
 title.innerHTML = titleSplit;
@@ -22,4 +25,47 @@ tl.from('#visual p', {
     opacity: 0,
     y: 150,
     duration: 1
+})
+
+//section offsetTop 배열 생성
+section.forEach((el, idx)=>{
+    sectionOffset.push(el.offsetTop);
+})
+console.log(sectionOffset[1]);
+
+
+//window.scroll 값 넣기
+window.addEventListener('scroll', e=>{
+    const scrollValue = window.scrollY;
+    scrollBox.innerHTML = scrollValue;
+    let numValue = (scrollValue - sectionOffset[1]) * 0.2;
+    const base = -200;
+
+    if(scrollValue >= sectionOffset[1]+base){
+        const tl = gsap.timeline();
+        tl.to('.des1 h2', {
+            y: 0,
+            opacity: 1,
+            duration: 0.6
+        })
+        tl.to('.des1 p', {
+            y: 0,
+            opacity: 1,
+            duration: 0.6
+        })
+        tl.to('.des2 h2', {
+            y: 0,
+            opacity: 1,
+            duration: 0.6
+        })
+        tl.to('.des2 p', {
+            y: 0,
+            opacity: 1,
+            duration: 0.6
+        })
+
+        for(let el of num){
+            el.style.transform = `translateY(${numValue}px)`
+        }
+    }
 })
